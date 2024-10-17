be11f1330f8c7083ef0c9f0a4954b66d5e5caddd
josemanuel11sans <20233tn078@utez.edu.mx>
2024-10-16 19:22:41 -0600

    aliminacion de tareas, y se muestra cantidad

--- Front/F/js/EstadoBoton.js
+++ Front/F/js/EstadoBoton.js
@@ -1,9 +1,9 @@
 //maneja el cambio de color del boton encendido y apagado que esta dentro de el html
  $(document).on('click', '.estado-toggle', function() {
-    var button = $(this);
+    let button = $(this);
     if (button.hasClass('btn-success')) {
-        button.removeClass('btn-success').addClass('btn-danger').text('Apagado');
+        button.removeClass('btn-success').addClass('btn-danger').text('Pendiente');
     } else {
-        button.removeClass('btn-danger').addClass('btn-success').text('Encendido');
+        button.removeClass('btn-danger').addClass('btn-success').text('Terminada');
     }
 });
